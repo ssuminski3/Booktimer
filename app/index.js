b168a510ed902b8ea8a489d27e3f5e029d37faa1
ssuminski3 <ssuminski3@gmail.com>
2024-07-09 15:06:58 +0200

    First commit where everything works

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+   <View className="flex-1 items-center justify-center bg-amber-100">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  );
+}
+
